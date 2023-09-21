e7e51887c0e46eeb5e8a54c64558c203d9c2bed6
cmpacheco23 <cmpachecomuevecela@gmail.com>
2023-09-21 12:11:52 -0400

    adjust profile model to reference food

--- models/profile.js
+++ models/profile.js
@@ -5,6 +5,7 @@ const Schema = mongoose.Schema
 const profileSchema = new Schema({
   name: String,
   avatar: String,
+  foods: [{type: Schema.Types.ObjectId, ref: 'Food'}]
 }, {
   timestamps: true
 })
