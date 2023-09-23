6618ec39d4ab4a8676aa656203ea7230656961af
cmpacheco23 <cmpachecomuevecela@gmail.com>
2023-09-23 15:48:01 -0400

    create profiles view and controller function

--- controllers/profiles.js
+++ controllers/profiles.js
@@ -3,8 +3,21 @@ import { populate } from "dotenv";
 import { Profile } from "../models/profile.js";
 
 function index(req, res){
-
+  //find all profiles
+  Profile.find({})
+  .then(profiles => {
+    //render view
+    res.render('profiles/index', {
+      profiles,
+      title: 'View All Profiles'
+    })
+  })
+  .catch(err => {
+    console.log(err)
+    res.redirect('/')
+  })
 }
+
 export {
   index,
 }
